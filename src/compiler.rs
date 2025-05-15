49a26b6a589d05c6e2e5e7aad6844f0f3ed24e85
Breval Ferrari <breval.ferrari@fish.golf>
2025-05-15 14:00:16 +0200

    add Clone derive to Instruction and IC

--- src/compiler.rs
+++ src/compiler.rs
@@ -50,7 +50,7 @@ macro_rules! bool_to_f64 {
 pub struct InstructionI(pub usize);
 
 /// This enumeration boosts performance because it eliminates expensive function calls for constant values.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum IC {
     I(InstructionI),
     C(f64),
@@ -77,7 +77,7 @@ macro_rules! ic_to_instr {
 }
 
 /// An `Instruction` is an optimized AST node resulting from compilation.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Instruction {
     //---- Primitive Value Types:
     IConst(f64),
